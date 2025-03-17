b765cf72e25f8cefd22c954c521758e24d98ed01
maxi <arora.anubhav93@gmail.com>
2025-03-17 17:27:07 +0530

    Setup ui actions to start and stop the service

--- app/src/main/java/com/almax/simplebackgroundservice/MainActivity.kt
+++ app/src/main/java/com/almax/simplebackgroundservice/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.almax.simplebackgroundservice
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.almax.simplebackgroundservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupUi()
+    }
+
+    private fun setupUi() {
+        binding.apply {
+            btnStart.setOnClickListener {
+                startService(Intent(this@MainActivity, BackgroundService::class.java))
+            }
+
+            btnStop.setOnClickListener {
+                stopService(Intent(this@MainActivity, BackgroundService::class.java))
+            }
+        }
     }
 }
